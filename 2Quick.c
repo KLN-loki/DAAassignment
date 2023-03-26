70bf72794ed3d75e342d87c924461c42b48d280b
KLN <95397474+KLN-loki@users.noreply.github.com>
2023-03-26 15:39:52 +0530

    Update 2Quick.c

--- 2Quick.c
+++ 2Quick.c
@@ -63,6 +63,17 @@ int main()
 
 // Output:
 // Given array is:
-// Srinivas Lakshmi Gopi Krishna Rama Partha 
+// Srinivas 
+// Lakshmi 
+// Gopi 
+// Krishna 
+// Rama 
+// Partha
+
 // Sorted array in Dictionary Order:
-// Gopi Krishna Lakshmi Partha Rama Srinivas
+// Gopi 
+// Krishna 
+// Lakshmi 
+// Partha 
+// Rama 
+// Srinivas
